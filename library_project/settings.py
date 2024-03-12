9655f00be70f713bb04b9c5950f4350476ed6696
Alokhon <aloatahanova@gmail.com>
2024-03-12 11:14:13 +0500

    gunicorn o'tnatildi

--- library_project/settings.py
+++ library_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 # from django.contrib.auth.middleware import AuthenticationMiddleware
 
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bcs02xmtxe%9qx4@_m!@y7twtiqo&dxr&g!gzpvnfl&b4m__*=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     # 'allauth.account',
     # 'allauth.socialaccount',
     'dj_rest_auth.registration',
+
 
 ]
 
@@ -144,6 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
